3d69955d695baf5066bb444202d956881f7d943f
Christopher Arthaud <christopher.arthaud@gmail.com>
2024-01-16 20:41:20 +0100

    Component folder created

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import BeautUI from "./Components/BeautUI";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/beautui",
+    element: <BeautUI />,
   },
 ]);
 

fb9980073764109af0c80b48b881d56f0e052782
Pat <hoang.patrick@outlook.com>
2024-01-18 17:33:08 +0100

    modification CSS page routine

--- src/Components/RoutinePage/RoutinePage.jsx
+++ src/Components/RoutinePage/RoutinePage.jsx
@@ -7,7 +7,7 @@ import * as React from "react";
 import Popover from "@mui/material/Popover";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import basket from "../../assets/basket.png"
+import basket from "../../assets/basket.png";
 
 export default function RoutinePage() {
   const [reponseDemaquillant, setReponseDemaquillant] = useState();
@@ -71,7 +71,7 @@ export default function RoutinePage() {
   }
 
   const openai = new OpenAI({
-    apiKey: "",
+    apiKey: "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",
     dangerouslyAllowBrowser: true,
   });
 
@@ -260,19 +260,10 @@ export default function RoutinePage() {
   return (
     <section className={styles.GlobalPage}>
       <div className={styles.topPage}>
-      <h2>Voici votre routine personnalisée</h2>
-      <Button
-            aria-describedby={id}
-            variant="contained"
-            onClick={handleClick}
-            className={styles.basketContainer}
-          >
-            <img src={basket} className={styles.basketImage} alt="" />
-          </Button> 
-          </div>
+        <h3>Voici votre routine personnalisée</h3>
+      </div>
       <div>
         <div>
-
           <Popover
             id={id}
             open={open}
@@ -284,11 +275,11 @@ export default function RoutinePage() {
             }}
           >
             <Typography className={styles.panierContainer}>
-            <h2>Votre panier</h2>
+              <h2>Votre panier</h2>
             </Typography>
             <Typography sx={{ p: 2 }} className={styles.productPanier}>
               {panier.map((product, index) => (
-                <ProductCard 
+                <ProductCard
                   key={index}
                   nom={product.nom && product.nom}
                   prix={product.prix && product.prix}
@@ -305,158 +296,172 @@ export default function RoutinePage() {
       reponseContour &&
       reponseCreme &&
       reponseLevres ? (
-        <div>
-          <div>
-            <h4 className={styles.titleContainer}>Etape 1 : Démaquillage</h4>
-            <p className={styles.descriptionContainer}>
-              Maquillée ou non, il faut toujours procéder à un nettoyage du
-              visage. Le démaquillage est donc une étape non négociable! Il
-              permet de désincruster non seulement le maquillage, mais aussi
-              tous les polluants et autres saletés que l’on accumule la journée
-              dans les transports, la rue, etc.
-            </p>
-            <div className={styles.cardPosition}>
-              <ProductCard
-                nom={reponseDemaquillant.nom && reponseDemaquillant.nom}
-                conseils_utilisation={
-                  reponseDemaquillant.raison && reponseDemaquillant.raison
-                }
-                prix={reponseDemaquillant.prix && reponseDemaquillant.prix}
-              />
-            </div>
+        <section className={styles.routinePageContainer}>
+          {" "}
+          <div className={styles.topPageButton}>
+            <Button
+              aria-describedby={id}
+              variant="contained"
+              onClick={handleClick}
+              className={styles.basketContainer}
+            >
+              <img src={basket} className={styles.basketImage} alt="" />
+            </Button>
           </div>
+          <div className={styles.routinePageContainer}>
+            <div className={styles.divContainer}>
+              <h4 className={styles.titleContainer}>Etape 1 : Démaquillage</h4>
+              <p className={styles.descriptionContainer}>
+                Maquillée ou non, il faut toujours procéder à un nettoyage du
+                visage. Le démaquillage est donc une étape non négociable! Il
+                permet de désincruster non seulement le maquillage, mais aussi
+                tous les polluants et autres saletés que l’on accumule la
+                journée dans les transports, la rue, etc.
+              </p>
+              <div className={styles.cardPosition}>
+                <ProductCard
+                  nom={reponseDemaquillant.nom && reponseDemaquillant.nom}
+                  conseils_utilisation={
+                    reponseDemaquillant.raison && reponseDemaquillant.raison
+                  }
+                  prix={reponseDemaquillant.prix && reponseDemaquillant.prix}
+                />
+              </div>
+            </div>
 
-          <div>
-            <h4 className={styles.titleContainer}>
-              Etape 2 : Nettoyer sa peau
-            </h4>
-            <p className={styles.descriptionContainer}>
-              Cette étape est essentielle pour obtenir une belle peau. Il faut
-              obligatoirement retirer les résidus d’huile, de maquillage ou
-              encore des cosmétiques laisser poser la nuit à l’aide d’un
-              nettoyant visage sous peine de boucher vos pores et créer des
-              imperfections.
-            </p>
-            <div className={styles.cardPosition}>
-              <ProductCard
-                nom={reponseNettoyant.nom && reponseNettoyant.nom}
-                conseils_utilisation={
-                  reponseNettoyant.raison && reponseNettoyant.raison
-                }
-                prix={reponseNettoyant.prix && reponseNettoyant.prix}
-              />
+            <div className={styles.divContainer}>
+              <h4 className={styles.titleContainer}>
+                Etape 2 : Nettoyer sa peau
+              </h4>
+              <p className={styles.descriptionContainer}>
+                Cette étape est essentielle pour obtenir une belle peau. Il faut
+                obligatoirement retirer les résidus d’huile, de maquillage ou
+                encore des cosmétiques laisser poser la nuit à l’aide d’un
+                nettoyant visage sous peine de boucher vos pores et créer des
+                imperfections.
+              </p>
+              <div className={styles.cardPosition}>
+                <ProductCard
+                  nom={reponseNettoyant.nom && reponseNettoyant.nom}
+                  conseils_utilisation={
+                    reponseNettoyant.raison && reponseNettoyant.raison
+                  }
+                  prix={reponseNettoyant.prix && reponseNettoyant.prix}
+                />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <h4 className={styles.titleContainer}>
-              Etape 3 : Appliquer une lotion
-            </h4>
-            <p className={styles.descriptionContainer}>
-              Lorsque l’on rince sa peau à l’eau, une fine couche de calcaire
-              vient se coller à l’épiderme. Résultat ? Beaucoup de personnes
-              ressentent des tiraillements, sensation de sécheresse et inconfort
-              après avoir nettoyer leur visage. Pour contrer cet effet négatif,
-              il faut rééquilibrer la peau et la rincer du calcaire en
-              appliquant une lotion tonique.
-            </p>
-            <div className={styles.cardPosition}>
-              <ProductCard
-                nom={reponseLotion.nom && reponseLotion.nom}
-                conseils_utilisation={
-                  reponseLotion.raison && reponseLotion.raison
-                }
-                prix={reponseLotion.prix && reponseLotion.prix}
-              />
+            <div className={styles.divContainer}>
+              <h4 className={styles.titleContainer}>
+                Etape 3 : Appliquer une lotion
+              </h4>
+              <p className={styles.descriptionContainer}>
+                Lorsque l’on rince sa peau à l’eau, une fine couche de calcaire
+                vient se coller à l’épiderme. Résultat ? Beaucoup de personnes
+                ressentent des tiraillements, sensation de sécheresse et
+                inconfort après avoir nettoyer leur visage. Pour contrer cet
+                effet négatif, il faut rééquilibrer la peau et la rincer du
+                calcaire en appliquant une lotion tonique.
+              </p>
+              <div className={styles.cardPosition}>
+                <ProductCard
+                  nom={reponseLotion.nom && reponseLotion.nom}
+                  conseils_utilisation={
+                    reponseLotion.raison && reponseLotion.raison
+                  }
+                  prix={reponseLotion.prix && reponseLotion.prix}
+                />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <h4 className={styles.titleContainer}>
-              Etape 4 : Traiter sa peau avec un sérum
-            </h4>
-            <p className={styles.descriptionContainer}>
-              Le sérum est un soin traitant concentré, riche en actif. Il va
-              traiter en profondeur la problématique que vous avez et compléter
-              l’action de la crème hydratante. Il est léger et pénètre
-              rapidement la peau, aucun risque de finir luisante avec lui 😉 Il
-              s’applique avant la crème hydratante.
-            </p>
-            <div className={styles.cardPosition}>
-              <ProductCard
-                nom={reponseSerum.nom && reponseSerum.nom}
-                conseils_utilisation={
-                  reponseSerum.raison && reponseSerum.raison
-                }
-                prix={reponseSerum.prix && reponseSerum.prix}
-              />
+            <div className={styles.divContainer}>
+              <h4 className={styles.titleContainer}>
+                Etape 4 : Traiter sa peau avec un sérum
+              </h4>
+              <p className={styles.descriptionContainer}>
+                Le sérum est un soin traitant concentré, riche en actif. Il va
+                traiter en profondeur la problématique que vous avez et
+                compléter l’action de la crème hydratante. Il est léger et
+                pénètre rapidement la peau, aucun risque de finir luisante avec
+                lui 😉 Il s’applique avant la crème hydratante.
+              </p>
+              <div className={styles.cardPosition}>
+                <ProductCard
+                  nom={reponseSerum.nom && reponseSerum.nom}
+                  conseils_utilisation={
+                    reponseSerum.raison && reponseSerum.raison
+                  }
+                  prix={reponseSerum.prix && reponseSerum.prix}
+                />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <h4 className={styles.titleContainer}>
-              Etape 5 : Contour des yeux
-            </h4>
-            <p className={styles.descriptionContainer}>
-              Non, il ne faut pas attendre les premières rides ou ridules de
-              déshydratation pour prendre soin de son contour de l’oeil. Souvent
-              mise à rude épreuve avec les correcteurs et le maquillage qu’on
-              lui impose, il est important de le traiter pour éviter de
-              l’abîmer.
-            </p>
-            <div className={styles.cardPosition}>
-              <ProductCard
-                nom={reponseContour.nom && reponseContour.nom}
-                conseils_utilisation={
-                  reponseContour.raison && reponseContour.raison
-                }
-                prix={reponseContour.prix && reponseContour.prix}
-              />
+            <div className={styles.divContainer}>
+              <h4 className={styles.titleContainer}>
+                Etape 5 : Contour des yeux
+              </h4>
+              <p className={styles.descriptionContainer}>
+                Non, il ne faut pas attendre les premières rides ou ridules de
+                déshydratation pour prendre soin de son contour de l’oeil.
+                Souvent mise à rude épreuve avec les correcteurs et le
+                maquillage qu’on lui impose, il est important de le traiter pour
+                éviter de l’abîmer.
+              </p>
+              <div className={styles.cardPosition}>
+                <ProductCard
+                  nom={reponseContour.nom && reponseContour.nom}
+                  conseils_utilisation={
+                    reponseContour.raison && reponseContour.raison
+                  }
+                  prix={reponseContour.prix && reponseContour.prix}
+                />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <h4 className={styles.titleContainer}>
-              Etape 6 : Hydrater la peau avec une crème hydratante
-            </h4>
-            <p className={styles.descriptionContainer}>
-              La crème hydratante va compléter l’action du sérum et apporter la
-              douceur et le confort à la peau. Vous pouvez également opter pour
-              un baume, une huile végétale simple ou une huile visage naturelle
-              en substitution. Seul votre ressenti et vos envies comptent 😉
-              Cette étape est indispensable le matin et le soir.
-            </p>
-            <div className={styles.cardPosition}>
-              <ProductCard
-                nom={reponseCreme.nom && reponseCreme.nom}
-                conseils_utilisation={
-                  reponseCreme.raison && reponseCreme.raison
-                }
-                prix={reponseCreme.prix && reponseCreme.prix}
-              />
+            <div className={styles.divContainer}>
+              <h4 className={styles.titleContainer}>
+                Etape 6 : Hydrater la peau avec une crème hydratante
+              </h4>
+              <p className={styles.descriptionContainer}>
+                La crème hydratante va compléter l’action du sérum et apporter
+                la douceur et le confort à la peau. Vous pouvez également opter
+                pour un baume, une huile végétale simple ou une huile visage
+                naturelle en substitution. Seul votre ressenti et vos envies
+                comptent 😉 Cette étape est indispensable le matin et le soir.
+              </p>
+              <div className={styles.cardPosition}>
+                <ProductCard
+                  nom={reponseCreme.nom && reponseCreme.nom}
+                  conseils_utilisation={
+                    reponseCreme.raison && reponseCreme.raison
+                  }
+                  prix={reponseCreme.prix && reponseCreme.prix}
+                />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <h4 className={styles.titleContainer}>
-              Etape 7 : Hydratation des lèvres
-            </h4>
-            <p className={styles.descriptionContainer}>
-              Les lèvres ne sont pas à négliger. Vous n’appliquerez pas un rouge
-              à lèvres sur des lèvres sèches et abîmées ! Il faut donc les
-              hydrater et les nourrir pour qu’elles restent douces et lisses.
-            </p>
-            <div className={styles.cardPosition}>
-              <ProductCard
-                nom={reponseLevres.nom && reponseLevres.nom}
-                conseils_utilisation={
-                  reponseLevres.raison && reponseLevres.raison
-                }
-                prix={reponseLevres.prix && reponseLevres.prix}
-              />
+            <div className={styles.divContainer}>
+              <h4 className={styles.titleContainer}>
+                Etape 7 : Hydratation des lèvres
+              </h4>
+              <p className={styles.descriptionContainer}>
+                Les lèvres ne sont pas à négliger. Vous n’appliquerez pas un
+                rouge à lèvres sur des lèvres sèches et abîmées ! Il faut donc
+                les hydrater et les nourrir pour qu’elles restent douces et
+                lisses.
+              </p>
+              <div className={styles.cardPosition}>
+                <ProductCard
+                  nom={reponseLevres.nom && reponseLevres.nom}
+                  conseils_utilisation={
+                    reponseLevres.raison && reponseLevres.raison
+                  }
+                  prix={reponseLevres.prix && reponseLevres.prix}
+                />
+              </div>
             </div>
           </div>
-        </div>
+        </section>
       ) : (
         <p>En cours ...</p>
       )}
